179310d4107d88d7002e894d97debcda79ca0738
luigithoriq <128014421+luigithoriq23@users.noreply.github.com>
2023-08-01 19:38:31 +0700

    Add files via upload

---
+++ PERTEMUAN KE 16 TUGAS BESAR UAS PERBAIKAN/SewaKamar/AppMultiLogin/Login.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Login
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Login))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Registrasi = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Yellow
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(51, 82)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Username"
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUsername.Location = New System.Drawing.Point(155, 79)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(138, 30)
+        Me.txtUsername.TabIndex = 1
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPassword.Location = New System.Drawing.Point(155, 129)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(138, 30)
+        Me.txtPassword.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Yellow
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(51, 132)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 25)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Password"
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.BackColor = System.Drawing.Color.GreenYellow
+        Me.btnSubmit.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSubmit.Location = New System.Drawing.Point(49, 181)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(243, 36)
+        Me.btnSubmit.TabIndex = 4
+        Me.btnSubmit.Text = "Sign In"
+        Me.btnSubmit.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.DarkGray
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label3.Location = New System.Drawing.Point(98, 34)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(155, 24)
+        Me.Label3.TabIndex = 59
+        Me.Label3.Text = "Penjualan Barang"
+        '
+        'Registrasi
+        '
+        Me.Registrasi.BackColor = System.Drawing.Color.RoyalBlue
+        Me.Registrasi.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Registrasi.Location = New System.Drawing.Point(49, 223)
+        Me.Registrasi.Name = "Registrasi"
+        Me.Registrasi.Size = New System.Drawing.Size(243, 36)
+        Me.Registrasi.TabIndex = 60
+        Me.Registrasi.Text = "Registrasi"
+        Me.Registrasi.UseVisualStyleBackColor = False
+        '
+        'Login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(343, 281)
+        Me.Controls.Add(Me.Registrasi)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Login"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Registrasi As Button
+End Class
---
+++ PERTEMUAN KE 16 TUGAS BESAR UAS PERBAIKAN/SewaKamar/AppMultiLogin/Penyewa.vb
@@ -0,0 +1,10 @@
+﻿Public Class Penyewa
+    Public Property id_penyewa As String
+    Public Property nama_penyewa As String
+    Public Property telepon As String
+    Public Property id_kamar As String
+    Public Property harga As String
+    Public Property jumlah_malam As String
+    Public Property total_biaya As String
+
+End Class
